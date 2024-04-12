e870f6b57a33b30741ecf9d8727e715e4a47142f
C2-H6 <60736641+C2-H6@users.noreply.github.com>
2024-04-12 16:10:16 +0200

    Time: 3 ms (53.77%) | Memory: 5.6 MB (50.67%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.c
@@ -0,0 +1,11 @@
+int timeRequiredToBuy(int* tickets, int ticketsSize, int k) 
+{
+    int time = 0;
+
+    for (int i = 0; i <= k; ++i)
+        time += (tickets[i] < tickets[k] ? tickets[i] : tickets[k]);
+
+    for (int i = k + 1; i < ticketsSize; ++i)
+        time += (tickets[i] < tickets[k] ? tickets[i]: tickets[k] - 1);
+    return time;
+}
